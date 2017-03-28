cd9204b53ae9b197e161dbf946c316908a507ebd
laurelKombo <girolaurel@gmail.com>
2017-03-28 11:23:14 +0200

    Update MyAccountController.cs

--- Project_Assesment/Controllers/MyAccountController.cs
+++ Project_Assesment/Controllers/MyAccountController.cs
@@ -19,34 +19,34 @@ namespace Project_Assesment.Controllers
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(UserClienSite client, string ReturnUrl = "")
-        {
+            //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Login(UserClienSite client, string ReturnUrl = "")
+        //{
 
-            if (ModelState.IsValid)
-            {
-                bool isValidUser = Membership.ValidateUser(client.Username, client.Password);
-                if (isValidUser)
-                {
-                    UserClienSite user = null;
-                    user = Repositories.UserRepository.findOne(client.Username, client.Password);
+        //    if (ModelState.IsValid)
+        //    {
+        //        bool isValidUser = Membership.ValidateUser(client.Username, client.Password);
+        //        if (isValidUser)
+        //        {
+        //            UserClienSite user = null;
+        //            user = Repositories.UserRepository.findOne(client.Username, client.Password);
 
-                    if (user != null)
-                    {
-                        JavaScriptSerializer js = new JavaScriptSerializer();
-                        string data = js.Serialize(user);
-                        FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, user.Username, DateTime.Now, DateTime.Now.AddMinutes(30), client.RememberMe, data);
-                        string encToken = FormsAuthentication.Encrypt(ticket);
-                        HttpCookie authoCookies = new HttpCookie(FormsAuthentication.FormsCookieName, encToken);
-                        Response.Cookies.Add(authoCookies);
-                        return Redirect(ReturnUrl);
-                    }
-                }
-            }
-            ModelState.Remove("Password");
-            return View();
-        }
+        //            if (user != null)
+        //            {
+        //                JavaScriptSerializer js = new JavaScriptSerializer();
+        //                string data = js.Serialize(user);
+        //                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, user.Username, DateTime.Now, DateTime.Now.AddMinutes(30), client.RememberMe, data);
+        //                string encToken = FormsAuthentication.Encrypt(ticket);
+        //                HttpCookie authoCookies = new HttpCookie(FormsAuthentication.FormsCookieName, encToken);
+        //                Response.Cookies.Add(authoCookies);
+        //                return Redirect(ReturnUrl);
+        //            }
+        //        }
+        //    }
+        //    ModelState.Remove("Password");
+        //    return View();
+        //}
 
         public ActionResult Logout()
         {
